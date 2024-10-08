a344eb967b9e471a9844eb87954e0de9f9fd635f
wstodgell <curtisstodgell2@gmail.com>
2024-10-08 12:53:11 -0700

    added polling

--- CDK/lambdas/attach_volume.py
+++ CDK/lambdas/attach_volume.py
@@ -1,16 +1,28 @@
 import boto3
+import time
 
 def get_parameter(name):
     ssm = boto3.client('ssm')
     return ssm.get_parameter(Name=name)['Parameter']['Value']
 
+def wait_for_instance_running(instance_id):
+    ec2 = boto3.client('ec2')
+    while True:
+        response = ec2.describe_instance_status(InstanceIds=[instance_id])
+        statuses = response['InstanceStatuses']
+        if statuses and statuses[0]['InstanceState']['Name'] == 'running':
+            return
+        time.sleep(5)  # Wait 5 seconds before checking again
+
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
     volume_id = get_parameter('/ai-model/volume-id')
-    instance_id = event['instance_id']  # Retrieve the instance_id from the previous step
+    instance_id = event['instance_id']
     
-    # Attach the volume
+    # Wait for the instance to be in the 'running' state
+    wait_for_instance_running(instance_id)
+    
+    # Attach the volume once the instance is running
     ec2.attach_volume(VolumeId=volume_id, InstanceId=instance_id, Device='/dev/sdh')
     
-    # Return both the instance_id and status for the next Lambda function
     return {'status': 'volume_attached', 'instance_id': instance_id}
