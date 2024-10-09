ea534741dee02ec7d7d6e618730437f4994c56e2
wstodgell <curtisstodgell2@gmail.com>
2024-10-09 11:35:43 -0700

    readded keypair

--- CDK/lib/ec2-stack.ts
+++ CDK/lib/ec2-stack.ts
@@ -249,5 +249,14 @@ export class EC2Stack extends cdk.Stack {
     });
     
     updateStopLambda.addToRolePolicy(lambdaPermissions);
+
+    const key = new ec2.CfnKeyPair(this, 'MyKeyPair', {
+      keyName: 'my-key-pair',
+    });
+    
+    new cdk.CfnOutput(this, 'KeyPairName', {
+      value: key.keyName,
+      description: 'Key Pair for SSH access',
+    });
   }
 }
